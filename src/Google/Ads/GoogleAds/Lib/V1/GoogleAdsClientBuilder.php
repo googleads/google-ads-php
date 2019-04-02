a2e5e6141d434b3125f442e1e82e4f82be75a5fa
Mattia Tommasone <mattiat@google.com>
2019-04-02 11:23:59 +0100

    Restored previous default value notation for fromFile
    Change-Id: Ibaa362c24c9193be3ad5f8df0a8d0829dbb074f9

--- src/Google/Ads/GoogleAds/Lib/V1/GoogleAdsClientBuilder.php
+++ src/Google/Ads/GoogleAds/Lib/V1/GoogleAdsClientBuilder.php
@@ -60,7 +60,7 @@ final class GoogleAdsClientBuilder implements GoogleAdsBuilder
      * @throws InvalidArgumentException if the configuration file could not be
      *     found
      */
-    public function fromFile(?string $path)
+    public function fromFile(string $path = null)
     {
         if ($path === null) {
             $path = self::DEFAULT_CONFIGURATION_FILENAME;
