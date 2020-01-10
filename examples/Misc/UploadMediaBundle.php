bcb2819a6884c398f747b3f10ed1dd65a03fb078
Pierrick Voulet <6769971+PierrickVoulet@users.noreply.github.com>
2020-01-10 10:59:38 -0500

    Add UploadMediaBundle code example (#255)
    * Add UploadMediaBundle code example
    
    Change-Id: I5df08c7f4252ae724114064b82389932348c8361
    
    * Switch to a single result log
    
    Change-Id: I5eef06bbd4495651c73b9ff007ab0df6745d3401
    
    * Remove useless field
    
    Change-Id: Ibed27ef59786f55ae4e13f5ea807ba0a464ff80e

---
+++ examples/Misc/UploadMediaBundle.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Ads\GoogleAds\Examples\Misc;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use GetOpt\GetOpt;
+use Google\Ads\GoogleAds\Examples\Utils\ArgumentNames;
+use Google\Ads\GoogleAds\Examples\Utils\ArgumentParser;
+use Google\Ads\GoogleAds\Lib\V2\GoogleAdsClient;
+use Google\Ads\GoogleAds\Lib\V2\GoogleAdsClientBuilder;
+use Google\Ads\GoogleAds\Lib\V2\GoogleAdsException;
+use Google\Ads\GoogleAds\Lib\OAuth2TokenBuilder;
+use Google\Ads\GoogleAds\V2\Enums\MediaTypeEnum\MediaType;
+use Google\Ads\GoogleAds\V2\Errors\GoogleAdsError;
+use Google\Ads\GoogleAds\V2\Resources\MediaBundle;
+use Google\Ads\GoogleAds\V2\Resources\MediaFile;
+use Google\Ads\GoogleAds\V2\Services\MediaFileOperation;
+use Google\ApiCore\ApiException;
+use Google\Protobuf\StringValue;
+
+/** This example uploads an HTML5 zip file as a media bundle. */
+class UploadMediaBundle
+{
+    const CUSTOMER_ID = 'INSERT_CUSTOMER_ID_HERE';
+    const BUNDLE_URL = 'https://goo.gl/9Y7qI2';
+
+    public static function main()
+    {
+        // Either pass the required parameters for this example on the command line, or insert them
+        // into the constants above.
+        $options = (new ArgumentParser())->parseCommandArguments([
+            ArgumentNames::CUSTOMER_ID => GetOpt::REQUIRED_ARGUMENT
+        ]);
+
+        // Generate a refreshable OAuth2 credential for authentication.
+        $oAuth2Credential = (new OAuth2TokenBuilder())->fromFile()->build();
+
+        // Construct a Google Ads client configured from a properties file and the
+        // OAuth2 credentials above.
+        $googleAdsClient = (new GoogleAdsClientBuilder())
+            ->fromFile()
+            ->withOAuth2Credential($oAuth2Credential)
+            ->build();
+
+        try {
+            self::runExample(
+                $googleAdsClient,
+                $options[ArgumentNames::CUSTOMER_ID] ?: self::CUSTOMER_ID
+            );
+        } catch (GoogleAdsException $googleAdsException) {
+            printf(
+                "Request with ID '%s' has failed.%sGoogle Ads failure details:%s",
+                $googleAdsException->getRequestId(),
+                PHP_EOL,
+                PHP_EOL
+            );
+            foreach ($googleAdsException->getGoogleAdsFailure()->getErrors() as $error) {
+                /** @var GoogleAdsError $error */
+                printf(
+                    "\t%s: %s%s",
+                    $error->getErrorCode()->getErrorCode(),
+                    $error->getMessage(),
+                    PHP_EOL
+                );
+            }
+        } catch (ApiException $apiException) {
+            printf(
+                "ApiException was thrown with message '%s'.%s",
+                $apiException->getMessage(),
+                PHP_EOL
+            );
+        }
+    }
+
+    /**
+     * Runs the example.
+     *
+     * @param GoogleAdsClient $googleAdsClient the Google Ads API client
+     * @param int $customerId the customer ID
+     */
+    public static function runExample(GoogleAdsClient $googleAdsClient, int $customerId)
+    {
+        // Creates an HTML5 zip file media bundle content.
+        $bundleContent = file_get_contents(self::BUNDLE_URL);
+
+        // Creates a media file.
+        $mediaFile = new MediaFile([
+            'name' => new StringValue(['value' => 'Ad Media Bundle']),
+            'type' => MediaType::MEDIA_BUNDLE,
+            'media_bundle' => new MediaBundle(['data' => $bundleContent])
+        ]);
+
+        // Creates a media file operation.
+        $mediaFileOperation = new MediaFileOperation();
+        $mediaFileOperation->setCreate($mediaFile);
+
+        // Issues a mutate request to add the media file.
+        $mediaFileServiceClient = $googleAdsClient->getMediaFileServiceClient();
+        $response = $mediaFileServiceClient->mutateMediaFiles(
+            $customerId,
+            [$mediaFileOperation]
+        );
+
+        printf(
+            "The media bundle with resource name '%s' was added.%s",
+            $response->getResults()[0]->getResourceName(),
+            PHP_EOL
+        );
+    }
+}
+
+UploadMediaBundle::main();
