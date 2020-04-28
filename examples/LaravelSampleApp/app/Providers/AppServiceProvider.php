74c4d29e5167171ca0c69b3bbfe0dc45d2562400
pierrick <pierrick@google.com>
2020-04-28 15:59:19 -0400

    Rollback to Laravel Sample App Baseline
    Change-Id: Ic6687546c932451730bb679909b050b9bed9a6f3

--- examples/LaravelSampleApp/app/Providers/AppServiceProvider.php
+++ examples/LaravelSampleApp/app/Providers/AppServiceProvider.php
@@ -1,25 +1,7 @@
 <?php
 
-/**
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 namespace App\Providers;
 
-use Google\Ads\GoogleAds\Lib\V3\GoogleAdsClientBuilder;
-use Google\Ads\GoogleAds\Lib\OAuth2TokenBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -31,16 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Binds the Google Ads API client.
-        $this->app->singleton('Google\Ads\GoogleAds\Lib\V3\GoogleAdsClient', function () {
-            // Constructs a Google Ads API client configured from the properties file.
-            return (new GoogleAdsClientBuilder())
-                ->fromFile(config('app.google_ads_php_path'))
-                ->withOAuth2Credential((new OAuth2TokenBuilder())
-                    ->fromFile(config('app.google_ads_php_path'))
-                    ->build())
-                ->build();
-        });
+        //
     }
 
     /**
