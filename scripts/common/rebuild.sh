50dac3e8fb938962a4b9e85e2033d0ebb3eaa331
pierrick <pierrick@google.com>
2020-06-10 19:51:53 -0400

    Make sure common scripts can be run from anywhere
    Change-Id: I1f08655bf3eccb0cd0c2204f33cbd1622bfaa2ca

--- scripts/common/rebuild.sh
+++ scripts/common/rebuild.sh
@@ -5,7 +5,8 @@
 set -exou pipefail;
 
 # Initialize variables
-WORK_DIR=${WORK_DIR:-}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WORK_DIR=${WORK_DIR:-$(dirname $(dirname $DIR))}
 
 # Update the composer dependencies for the project.
 pushd "$WORK_DIR"
--- scripts/common/run_tests.sh
+++ scripts/common/run_tests.sh
@@ -5,8 +5,8 @@
 set -exou pipefail;
 
 # Initialize variables
-WORK_DIR=${WORK_DIR:-}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+WORK_DIR=${WORK_DIR:-$(dirname $(dirname $DIR))}
 
 # Rebuild
 
