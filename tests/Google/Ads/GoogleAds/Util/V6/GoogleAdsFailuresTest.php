953f31de093445e44ec554f37f36931262c9a4d5
pierrick <pierrick@google.com>
2020-12-09 20:39:29 -0500

    Fix additional psr-4 issues
    Change-Id: I83a6b1804d4666155cac3da7e346abdfbe13dfea

--- tests/Google/Ads/GoogleAds/Util/V6/GoogleAdsFailuresTest.php
+++ tests/Google/Ads/GoogleAds/Util/V6/GoogleAdsFailuresTest.php
@@ -24,7 +24,7 @@ use Google\Ads\GoogleAds\V6\Errors\GoogleAdsError;
 use Google\Ads\GoogleAds\V6\Errors\GoogleAdsFailure;
 use PHPUnit\Framework\TestCase;
 
-class GoogleAdsFailureTest extends TestCase
+class GoogleAdsFailuresTest extends TestCase
 {
     public function testFromAny()
     {
