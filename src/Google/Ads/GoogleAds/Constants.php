0bc0af83992f654e3b36bd0bdeccb5e3e0712771
Pierrick Voulet <6769971+PierrickVoulet@users.noreply.github.com>
2021-09-08 12:21:41 +0000

    Add library name and version in header (#643)
    * Add client library in the user agent of API requests
    
    Change-Id: I897995dbf6ae1b5fc4b7e09f02581d7a2314048c
    
    * Edit client library name
    
    Change-Id: I3e3ca2049449e9c788bdc2d4e71a2b063c23344d
    
    * Change for gacl key
    
    Change-Id: I64ed1fda12e6d161bf99e7003f98d2c36641c3f5
    
    * Change for gccl
    
    Change-Id: I1066bb70752c0886d57d7f7b3315ddc022c87387
    
    * Revamp the constant class and upgrade to latest version
    
    Change-Id: I0b8a3e91e2d807c802f98ac09981e5890ff80793
    
    * Added client library version in V7 and renamed the constants
    
    Change-Id: Ib3b4ac7a8e5fdc31efe78b65586ce48949e0b323
    
    Co-authored-by: pierrick <pierrick@google.com>

---
+++ src/Google/Ads/GoogleAds/Constants.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Ads\GoogleAds;
+
+/**
+ * Class to store constants and default values of the library.
+ */
+class Constants
+{
+    // The library name to use in Google's request headers.
+    public const LIBRARY_NAME = 'gccl';
+
+    // The library version.
+    public const LIBRARY_VERSION = '11.0.0';
+}
--- src/Google/Ads/GoogleAds/Lib/V8/ServiceClientFactoryTrait.php
+++ src/Google/Ads/GoogleAds/Lib/V8/ServiceClientFactoryTrait.php
@@ -20,6 +20,7 @@
 
 namespace Google\Ads\GoogleAds\Lib\V8;
 
+use Google\Ads\GoogleAds\Constants;
 use Google\Ads\GoogleAds\Lib\ConfigurationTrait;
 use Google\Ads\GoogleAds\V8\Services\AccessibleBiddingStrategyServiceClient;
 use Google\Ads\GoogleAds\V8\Services\AccountBudgetProposalServiceClient;
@@ -196,6 +197,8 @@ trait ServiceClientFactoryTrait
         if (!empty($this->getEndpoint())) {
             $clientOptions += [self::$SERVICE_ADDRESS_KEY => $this->getEndpoint()];
         }
+        $clientOptions['libName'] = Constants::LIBRARY_NAME;
+        $clientOptions['libVersion'] = Constants::LIBRARY_VERSION;
         $clientOptions['transportConfig'] = [
             'grpc' => [
                 'stubOpts' => [
