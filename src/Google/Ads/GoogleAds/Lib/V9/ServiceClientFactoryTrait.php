b3c8e6d1389a0decf3cd489a0badae8bd17c7d79
pierrick <pierrick@google.com>
2021-11-16 21:56:54 +0000

    Add support for V9
    Change-Id: I6f28460be952b1daa7e2f0a7dc67177bdefbdb6a

--- src/Google/Ads/GoogleAds/Lib/V9/ServiceClientFactoryTrait.php
+++ src/Google/Ads/GoogleAds/Lib/V9/ServiceClientFactoryTrait.php
@@ -240,6 +240,10 @@ trait ServiceClientFactoryTrait
                 $googleAdsLoggingInterceptor
             );
         }
+        array_push(
+            $clientOptions['transportConfig']['grpc']['interceptors'],
+            ...$this->getGrpcInterceptors()
+        );
         if (!empty($this->getProxy())) {
             putenv('http_proxy=' . $this->getProxy());
         }
