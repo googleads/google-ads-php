aa0860768ee78d1bf50fce20626622d0d9363a6f
Thanet Knack Praneenararat <fiboknacky@users.noreply.github.com>
2024-12-13 02:34:20 +0900

    Added support for PHP 8.4. (#1059)
    - Regenerated source code to suppress the deprecation warnings.
    - Updated src/Google/Ads/GoogleAds/Lib/InsecureCredentialsWrapper.php.
    
    Bug: N/A

--- src/Google/Ads/GoogleAds/V18/Services/Client/SmartCampaignSettingServiceClient.php
+++ src/Google/Ads/GoogleAds/V18/Services/Client/SmartCampaignSettingServiceClient.php
@@ -38,6 +38,7 @@ use Google\ApiCore\Transport\TransportInterface;
 use Google\ApiCore\ValidationException;
 use Google\Auth\FetchAuthTokenInterface;
 use GuzzleHttp\Promise\PromiseInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * Service Description: Service to manage Smart campaign settings.
@@ -50,8 +51,8 @@ use GuzzleHttp\Promise\PromiseInterface;
  * name, and additionally a parseName method to extract the individual identifiers
  * contained within formatted names that are returned by the API.
  *
- * @method PromiseInterface getSmartCampaignStatusAsync(GetSmartCampaignStatusRequest $request, array $optionalArgs = [])
- * @method PromiseInterface mutateSmartCampaignSettingsAsync(MutateSmartCampaignSettingsRequest $request, array $optionalArgs = [])
+ * @method PromiseInterface<GetSmartCampaignStatusResponse> getSmartCampaignStatusAsync(GetSmartCampaignStatusRequest $request, array $optionalArgs = [])
+ * @method PromiseInterface<MutateSmartCampaignSettingsResponse> mutateSmartCampaignSettingsAsync(MutateSmartCampaignSettingsRequest $request, array $optionalArgs = [])
  */
 class SmartCampaignSettingServiceClient
 {
@@ -152,14 +153,14 @@ class SmartCampaignSettingServiceClient
      * listed, then parseName will check each of the supported templates, and return
      * the first match.
      *
-     * @param string $formattedName The formatted name string
-     * @param string $template      Optional name of template to match
+     * @param string  $formattedName The formatted name string
+     * @param ?string $template      Optional name of template to match
      *
      * @return array An associative array from name component IDs to component values.
      *
      * @throws ValidationException If $formattedName could not be matched.
      */
-    public static function parseName(string $formattedName, string $template = null): array
+    public static function parseName(string $formattedName, ?string $template = null): array
     {
         return self::parseFormattedName($formattedName, $template);
     }
@@ -214,6 +215,9 @@ class SmartCampaignSettingServiceClient
      *     @type callable $clientCertSource
      *           A callable which returns the client cert as a string. This can be used to
      *           provide a certificate and private key to the transport layer for mTLS.
+     *     @type false|LoggerInterface $logger
+     *           A PSR-3 compliant logger. If set to false, logging is disabled, ignoring the
+     *           'GOOGLE_SDK_PHP_LOGGING' environment flag
      * }
      *
      * @throws ValidationException
